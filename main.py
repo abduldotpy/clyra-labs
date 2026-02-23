f115012b4444f7c51fe372711888210079d2a11d
abduldotpy <abdulsamadkhan1090@gmail.com>
2026-02-23 06:05:43 +0500

    initial Pipeline and langsmith integeration

--- main.py
+++ main.py
@@ -2,25 +2,47 @@ import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import ChatPromptTemplate
-# Load API key from .env file
+from langchain_core.output_parsers import StrOutputParser
+import requests
+parser = StrOutputParser()
+
 load_dotenv()
-# 1. Setup the model
-model = ChatGoogleGenerativeAI(
+
+model_text = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     google_api_key=os.getenv("GOOGLE_API_KEY"),
-    temperature=0.7,
-    stream=True
+    temperature=0.0    
 )
-# 2. Create a prompt template
+model_image = model_text = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-image" ,
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    temperature=0.0    
+)
+
+
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful assistant."),
+    ("system", """You are an expert prompt rewriter for image generation.
+      User will be input the prompt which could be needed some tweaking or no tweaking at all. You will be explaining the prompt further to enhance image generation
+     just enhance the given prompt dont add add questions or extra things"""),
     ("human", "{user_input}"),
 ])
-# 3. Create the chain
-chain = prompt | model
-# 4. Run it
-for chunk in chain.stream({"user_input": "Explain quantum computing in simple terms."}):
-    print(chunk.content, end="", flush=True)
 
-# New line after streaming finishes
-print()
+chain = prompt | model_text | parser | model_image
+
+# nostreaming
+response = chain.invoke({'What do you know about quatum computing'})
+# print(response.content)
+
+#streaming output
+# for chunk in chain.stream({"user_input": "Generate Image of the red car"}):
+#     print(chunk.content, end="", flush=True)
+
+
+# # New line after streaming finishes
+# print()
+
+
+image_url = response.content # or similar extraction
+image_data = requests.get(image_url).content
+with open("generated_image.png", "wb") as f:
+    f.write(image_data)
